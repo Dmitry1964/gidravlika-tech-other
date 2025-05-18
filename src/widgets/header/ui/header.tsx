66f9d6b769940b3bade7810efcba78814b325a26
Dmytry64 <nosdm64@gmail.com>
2025-05-18 13:23:37 +0300

    Добавляет мобильное меню

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,9 +1,17 @@
-import './header.module.scss';
+import { HeaderContacts } from "src/features/header-contacts";
+import "./header.module.scss";
+import { Navbar } from "src/features/navbar";
+import useViewportWidth from "src/hooks/useViewportWidth";
+import { TABLET_VIEWPORT } from "src/shared/lib/constans";
 
-const Header = () : React.ReactNode => {
-    return (
-        <header></header>
-    )
-}
+const Header = (): React.ReactNode => {
+  const screenWidth = useViewportWidth()
+  return (
+    <header>
+      <HeaderContacts />
+      {screenWidth >= TABLET_VIEWPORT && <Navbar />}
+    </header>
+  );
+};
 
 export default Header;
