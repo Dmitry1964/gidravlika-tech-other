c2493df5cd1ed880eadc46992bab6da41392174b
Dmytry64 <nosdm64@gmail.com>
2025-05-21 06:06:21 +0300

    Добавляет блок Intro

--- src/pages/main-page/ui/main-page.tsx
+++ src/pages/main-page/ui/main-page.tsx
@@ -1,11 +1,15 @@
 import { Banner } from "src/widgets/banner";
+import { Intro } from "src/widgets/intro";
 
-const MainPage = () : React.ReactNode => {
-    return (
-        <main>
-            <Banner />
-        </main>
-    )
+const MainPage = (): React.ReactNode => {
+  return (
+    <main>
+      <Banner />
+      <Intro>
+        <h2>УСЛУГИ НАШЕЙ КОМПАНИИ:</h2>
+      </Intro>
+    </main>
+  )
 }
 
 export default MainPage;
